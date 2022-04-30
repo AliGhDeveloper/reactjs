9c774eb1572c9eccc1a81bbbbd327a72c868beb4
Ali gh <ghasempour.2m@gmail.com>
2022-04-30 11:29:10 +0430

    Creating SiteFooter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navbar from './Navbar'
 import MainPage from './MainPage'
-
+import SiteFooter from './SiteFooter'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainPage />
+      <SiteFooter />
     </div>
   );
 }
---
+++ src/SiteFooter.js
@@ -0,0 +1,23 @@
+function SiteFooter(){
+    return(
+        <div className="SiteFooter">
+            <div className="refrences">
+                <span>Important Refrences</span>
+                <ul>
+                    <li><a href="#">MDN</a></li>
+                    <li><a href="#">W3School</a></li>
+                    <li><a href="#">YouTube</a></li>
+                </ul>
+            </div>
+            <div className="links">
+                <span>Important Links</span>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default SiteFooter;
